57c04cb385c746c713430006c1c4333bd1aac227
lispking <lispking@qq.com>
2023-08-09 01:00:58 +0800

    ! update sepolia contract address

--- scripts/networks/sepolia.ts
+++ scripts/networks/sepolia.ts
@@ -16,24 +16,27 @@ export async function makeConfig(): Promise<Config> {
         aavePoolAddress: '0xE7EC1B0015eb2ADEedb1B7f9F1Ce82F9DAD6dF08', // Pool-Proxy-Aave
         aTokenAddress: '0x177F4611B27Cb66f5E6A51B4DD956f37A75F883B', // AToken-Aave
         // https://docs.compound.finance/v2/#networks
-        cTokenAddress: '0x0545a8eaF7ff6bB6F708CbB544EA55DBc2ad7b2a', // cDAI
+        cTokenAddress: '0x0545a8eaF7ff6bB6F708CbB544EA55DBc2ad7b2a', // cDAI - not support this network
         // https://docs.uniswap.org/contracts/v3/reference/deployments
-        uniswapRouterAddress: '0xE592427A0AEce92De3Edee1F18E0157C05861564',
-        earningTokenAddress: '0x2899a03ffDab5C90BADc5920b4f53B0884EB13cC',// DAI
+        // https://sepolia.etherscan.io/address/0x0227628f3F023bb0B980b67D528571c95c6DaC1c
+        uniswapRouterAddress: '0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD',
+        // https://sepolia.etherscan.io/address/0xe7ec1b0015eb2adeedb1b7f9f1ce82f9dad6df08#readProxyContract
+        earningTokenAddress: '0x68194a729C2450ad26072b3D33ADaCbcef39D574',// DAI
     }
 
     return {
         systemConfig: systemConfig,
         
-        ethTokenAddress: '0xCCB14936C2E000ED8393A571D15A2672537838Ad', // WETH
-        daiTokenAddress: '0x2899a03ffDab5C90BADc5920b4f53B0884EB13cC',// DAI
+        // https://sepolia.etherscan.io/address/0xe7ec1b0015eb2adeedb1b7f9f1ce82f9dad6df08#readProxyContract
+        ethTokenAddress: '0xD0dF82dE051244f04BfF3A8bB1f62E1cD39eED92', // WETH
+        daiTokenAddress: '0x8a0E31de20651fe58A369fD6f76c21A8FF7f8d42',// LINK
     }
 }
 
 export async function deployEarningPoolContracts(config: Config, deployAaveEarningPool: Function, deployCompoundEarningPool: Function): Promise<{[key: string]: Contract}> {
     return {
         'WETH': await deployAaveEarningPool(config.ethTokenAddress),
-        'DAI': await deployCompoundEarningPool(config.daiTokenAddress),
+        'LINK': await deployCompoundEarningPool(config.daiTokenAddress),
     }
 }
 
@@ -47,9 +50,9 @@ export async function makeCombos(config: Config, earningPoolContracts: {[key: st
 
     const tokenLink: ComboEntry = makeCombo(70, {
         id: 1,
-        name: 'DAI',
+        name: 'LINK',
         token: config.daiTokenAddress,
-        earningContract: await earningPoolContracts.DAI.getAddress(),
+        earningContract: await earningPoolContracts.LINK.getAddress(),
     });
 
     return [{
