f49104fadb74d517628e9bbb66b29d801fb990cc
lispking <lispking@qq.com>
2023-08-14 09:56:57 +0800

    ! remove unused code

--- contracts/libraries/utils/ReentrancyGuard.sol
+++ contracts/libraries/utils/ReentrancyGuard.sol
@@ -56,12 +56,4 @@ abstract contract ReentrancyGuard is Initializable, ErrorReporter {
         // https://eips.ethereum.org/EIPS/eip-2200)
         _status = _NOT_ENTERED;
     }
-
-    /**
-     * @dev Returns true if the reentrancy guard is currently set to "entered", which indicates there is a
-     * `nonReentrant` function in the call stack.
-     */
-    function _reentrancyGuardEntered() internal view returns (bool) {
-        return _status == _ENTERED;
-    }
 }
